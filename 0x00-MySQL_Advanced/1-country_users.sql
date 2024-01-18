ec98bca310a9418f67e5a38a0e7a06c5039e9bdc
geno <yvesgeno@outlook.com>
2024-01-18 22:52:35 +0200

    task 1 correction

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,13 +4,9 @@
 -- name varchar(255)
 -- country enum('US', 'CO', 'TN') not null default 'US'
 
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-
-    CREATE TABLE users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255),
-        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-    );
-
-END IF;
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM ('US', 'CO', 'TN') NOT NULL
+);
