8a60171605e77c03141358028708dd2206c47485
geno <yvesgeno@outlook.com>
2024-01-25 00:19:07 +0200

    task 4 correction

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,22 +8,24 @@ from functools import wraps
 
 
 def replay(func: Callable):
-    """Replay decorator"""
-    key = func.__qualname__
+    """
+    Prototype: def replay(func: Callable):
+    Displays history of calls of a particular function
+    """
     r = redis.Redis()
-
-    inputs = r.lrange("{}:inputs".format(key), 0, -1)
-    outputs = r.lrange("{}:outputs".format(key), 0, -1)
-    calls = len(inputs)
-    times = 'times'
-    if calls == 1:
-        times = 'time'
-    out = '{} was called {} {}:'.format(key, calls, times)
-    print(out)
-    for i, o in zip(inputs, outputs):
-        out = '{}(*{}) -> {}'.format(key, i.decode('utf-8'),
-                                     o.decode('utf-8'))
-        print(out)
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m, k.decode('utf-8'), v.decode('utf-8'))
+        print(fin)
 
 
 def call_history(method: Callable) -> Callable:
